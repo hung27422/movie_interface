0e8e9e8a493c2a70bd787759734743ca3aff047e
Ho Tan Hung <tanhungho2002@gmail.com>
2024-07-03 18:07:05 +0700

    fix spinner and build ui search

--- src/app/pages/Search/page.tsx
+++ src/app/pages/Search/page.tsx
@@ -1,5 +1,14 @@
 function Search() {
-  return <h2>Search</h2>;
+  return (
+    <div>
+      <div className="w-full text-center py-2 bg-sidebar">
+        <input
+          type="text"
+          className="w-[60%] h-12 outline-none rounded-md text-white bg-page py-1 px-3 font-bold"
+        />
+      </div>
+    </div>
+  );
 }
 
 export default Search;
--- src/app/pages/Spinner/page.tsx
+++ src/app/pages/Spinner/page.tsx
@@ -1,3 +1,4 @@
+"use client";
 import { Blocks } from "react-loader-spinner";
 function Spinner() {
   return (
